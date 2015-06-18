e1b1f7ef4c859ba634d6caf4f5f7de33acb324af
gilbertliou <gilbert.liou.tw@gmail.com>
2015-06-18 23:45:59 +0800

    remove redundant files

--- test.sh
+++
@@ -1,7 +0,0 @@
-export LC_ALL="en_US.UTF-8"
-sudo apt-get update
-sudo apt-get install -y --force-yes build-essential python-dev gfortran python-pip
-sudo apt-get install -y --force-yes libblas-dev liblapack-dev liblapacke-dev
-sudo apt-get -y --force-yes upgrade
-sudo apt-get -y --force-yes install cython python-numpy python-scipy
-sudo pip install celery
